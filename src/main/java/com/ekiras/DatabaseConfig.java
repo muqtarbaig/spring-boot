0c889d4e59dd28d18eb47a3a5852ceec28ab2358
muqtarbaig <muqtarbaig@gmail.com>
2016-09-24 22:15:27 +0530

    First cut, spring-boot

---
+++ src/main/java/com/ekiras/DatabaseConfig.java
@@ -0,0 +1,13 @@
+//package com.ekiras;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DatabaseConfig {
+//
+//	/*public DataSource setDataSource() {
+//		
+//	} */
+//}
